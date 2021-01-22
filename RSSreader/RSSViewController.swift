34769ced7513de637c58eaa1de0662ce1094f656
Valeriy Pokatilo <ya_valerko@mail.ru>
2021-01-23 00:36:43 +0700

    Create start controller

--- RSSreader/RSSViewController.swift
+++ RSSreader/RSSViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RSSViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.purple
     }
 
 
